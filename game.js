34a8d1ec22ae084ae45e018601d0f7695f7f2f51
Master <nghiemxthai@gmail.com>
2024-02-20 03:36:56 -0800

    added logo and responsive

--- game.js
+++ game.js
@@ -1,8 +1,25 @@
-
+let star1Value;
+let star2Value;
+let star3Value;
+let star4Value;
 
 $('#start').on('click', ()=>{
     console.log('Clicked')
-   $('#WelcomeMode').remove() 
+   $('#WelcomeMode').hide()
    $('#GameMode').removeAttr("hidden")
-})
+});
 
+const getRandom = (num)=> {
+    return Math.floor(Math.random()*num)
+};
+
+$('#star1').html( star1Value || '???');
+$('#star2').html( star2Value || '???');
+$('#star3').html( star3Value || '???');
+$('#star4').html( star4Value || '???');
+
+$(document).ready(function(){
+    const getTargetScore = getRandom(40);
+    $('#target').html(getTargetScore)
+    
+});
